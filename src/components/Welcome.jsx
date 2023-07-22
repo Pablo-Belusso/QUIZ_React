c916de8a86a00e6a9e552641a33b142275b191a0
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-22 17:51:56 -0300

    Commit 05

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Seja Bem Vindo!</h2>
+      <p>Clique no Botão abaixo para começar:</p>
+    </div>
+  );
+};
+
+export default Welcome;
