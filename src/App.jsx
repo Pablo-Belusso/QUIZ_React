76cbec1f2c22c638901926e833f2c6aa97d4fb1e
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-26 13:51:55 -0300

    Commit 16

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import "./App.css";
+import { useContext } from "react";
+import { QuizContext } from "./context/quiz.jsx";
 import Welcome from "./components/Welcome.jsx";
+import "./App.css";
+
+//import Question from "./components/Question.jsx";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
   return (
     <div className="App">
-      <h1> Quizz Testandooooo </h1>
-      <Welcome></Welcome>
+      <h1> Quizz de Programação: </h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {/* {quizState.gameStage === "Playing" && <Question />} */}
     </div>
   );
 }
