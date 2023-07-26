75060697cfaeffb19c4599c903e1a5f4e53818f3
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-26 17:16:34 -0300

    Commit 20

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -8,12 +8,13 @@ const Question = () => {
 
   console.log(quizState);
   return (
-    <div>
+    <div id="question">
       <p>Pergunta de 0 a 10</p>
       <h2>Pergunta atual</h2>
       <div id="options-container">
         <p>Opções</p>
       </div>
+      <button>Continuar</button>
     </div>
   );
 };
